a576f4d9f486234334d0353c739dfa95690115ea
Wilson <wilson.chen@student.uts.edu.au>
2016-11-29 19:57:13 +1100

    cleanup

--- express/public/javascripts/code.js
+++ express/public/javascripts/code.js
@@ -1,55 +1,81 @@
 var $ = require('jquery');
 var _ = require('lodash');
 var joint = require('jointjs');
+const JSONString = {
+    "global": [
+        {
+            "variable": [
+                {
+                    "name": "x"
+                }
+            ]
+        }
+        , {
+            "variable": [
+                {
+                    "name": "y"
+                }
+            ]
+        }
+        , {
+            "function": [
+                {
+                    "name": "setPoint"
+                    , "parameter": "x,y"
+                    , "return": "null"
+                }
+            ]
+        }
 
-var el1 = new joint.shapes.basic.Rect({
-    position: {
-        x: 50
-        , y: 50
-    }
-    , size: {
-        width: 100
-        , height: 40
-    }
-    , attrs: {
-        rect: {
-            'stroke-width': '5'
-            , 'stroke-opacity': .7
-            , stroke: 'black'
-            , rx: 3
-            , ry: 3
-            , fill: 'lightgray'
-            , 'fill-opacity': .5
-        }
-        , text: {
-            text: 'Drop me over B'
-            , 'font-size': 10
-            , style: {
-                'text-shadow': '1px 1px 1px lightgray'
-            }
-        }
-    }
-});
+    ]
+}
 var paper;
 var graph;
 window.buildJoint = function () {
+    var selectedDiv = $('#paper-connection-by-dropping');
     if ((graph instanceof joint.dia.Graph) && (paper instanceof joint.dia.Paper)) {
-        graph.get("cells").forEach(
-            function(cell) {
-                paper.findViewByModel(cell).remove();
-            }
-        );
+        graph.get("cells").forEach(function (cell) {
+            paper.findViewByModel(cell).remove();
+        });
         $('#paper-connection-by-dropping').empty();
     }
     graph = new joint.dia.Graph;
     paper = new joint.dia.Paper({
-        el: $('#paper-connection-by-dropping')
-        , width: 650
-        , height: 200
+        el: selectedDiv
+        , width: selectedDiv.width()
+        , height: selectedDiv.height()
         , gridSize: 1
         , model: graph
     });
-    graph.addCells([el1, el1.clone().translate(200, 50).attr('text/text', 'B')]);
+    var el1 = new joint.shapes.basic.Rect({
+        position: {
+            x: 50
+            , y: 50
+        }
+        , size: {
+            width: 100
+            , height: 40
+        }
+        , attrs: {
+            rect: {
+                'stroke-width': '5'
+                , 'stroke-opacity': .7
+                , stroke: 'black'
+                , rx: 3
+                , ry: 3
+                , fill: 'lightgray'
+                , 'fill-opacity': .5
+            }
+            , text: {
+                text: 'Drop me over B'
+                , 'font-size': 10
+                , style: {
+                    'text-shadow': '1px 1px 1px lightgray'
+                }
+            }
+        }
+    });
+    graph.addCells([el1, el1.clone().translate(50, 50).attr('text/text', 'B')]);
     console.log(graph.getCells());
     // Here is the real deal. Listen on cell:pointerup and link to an element found below.
     paper.on('cell:pointerup', function (cellView, evt, x, y) {
@@ -89,5 +115,6 @@ window.output = function printBoxContents() {
     var parser = document.getElementById("vim").contentWindow.editor;
     var outputString = parser.getValue();
     console.log(esprima.parse(outputString));
+    console.log(JSONString);
     document.getElementById("output").innerHTML = JSON.stringify(esprima.parse(outputString), null, 2);
 }
